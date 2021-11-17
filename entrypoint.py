029be4b2e268a4d16b95f3be28f771f71a9a2a09
jhnnsrs <jhnnsrs@gmail.com>
2021-11-17 11:11:30 +0100

    d

--- entrypoint.py
+++ entrypoint.py
@@ -1,8 +1,23 @@
-from port import actify
+from arkitekt.actors.actify import define
+from mikro import Representation
+from yaml import dump, Dumper
+import yaml
 
 
-actify()
+def hallo(nana: Representation) -> Representation:
+    """Hallo
 
+    generates hallo
 
-def hallo():
+    Args:
+        nana (Representation): [description]
+
+    Returns:
+        Representation: [description]
+    """
     print("nenewnew")
+
+
+x = define(hallo)
+with open(".port/definition.yaml", "w") as f:
+    yaml.dump(x.dict(as_input=True), f)
